dd28f627ff568ee8e3184a14d13f302b5f89a8e0
LionelLints <lionel.lints@gmail.com>
2015-12-15 13:47:10 -0800

    update routes to include users surveys and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root :to => 'application#angular'
+  resource :sessions, only: [:create, :destroy]
+  resources :users , only: [:show, :create, :update, :destroy] do
+    resources :surveys, shallow: true
+  end
+
+  # resources :users, only [:show, :create, :update, :destroy] do
+  #   resources :surveys, only [:show, :create, :update, :destroy]
+  # end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
