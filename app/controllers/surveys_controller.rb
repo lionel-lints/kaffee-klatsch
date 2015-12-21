ca4092c5b8368d7b29c41d1f48583744fd30f580
LionelLints <lionel.lints@gmail.com>
2015-12-21 10:47:31 -0800

    remove unneccessary files and update surveys controller

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -18,52 +18,48 @@ class SurveysController < ApplicationController
   end
 
   def create
-
     location = Typhoeus.post('https://www.googleapis.com/geolocation/v1/geolocate?key=' + ENV['COFFEE_TIME_GOOGLE_GEOLOCATION'], body: {})
     location = JSON.parse location.response_body
     lat = location['location']['lat'].round(4)
     lon = location['location']['lng'].round(4)
     response = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?lat=' + lat.to_s + '&units=imperial&lon=' + lon.to_s + '&type=accurate&appid=' + ENV['COFFEE_TIME_APP'])
+
+    #put the response in a hash
     hash = JSON.parse response.body
 
+    p "Open Weather Response!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     p 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat.to_s + '&units=imperial&lon=' + lon.to_s + '&appid=' + ENV['COFFEE_TIME_APP']
+    p "Open Weather Response end!!!!!!!!!!!!!!!!!!!!!!"
 
     @user = User.find(params[:user_id])
+
+    #add location and weather data to the survey
     @survey = @user.surveys.create survey_params.merge(:weatherMain => hash['weather'][0]['main'], :weatherDesc => hash['weather'][0]['description'], :dayTempHi => hash['main']['temp_max'], :dayTempLow => hash['main']['temp_min'], :currentTemp => hash['main']['temp'])
-    @user = User.find(params[:user_id])
-    @survey = @user.surveys.create survey_params
+    # @user = User.find(params[:user_id])
+    # @survey = @user.surveys.create survey_params
 
-    respond_to do |format|
-      if @survey.save
-        format.html { redirect_to user_surveys_path(@user, @survey), notice: 'Survey was successfully created.' }
-        format.json { render :show, status: :created, location: @survey }
-      else
-        format.html { render :new }
-        format.json { render json: @survey.errors, status: :unprocessable_entity }
-      end
+    if @survey.save
+      #rerender the page with the new data?
+      p "Survey saved to DB!!!!!!!!!!!!!!!!!!!!!!"
+    else
+      p "Survey failed to DB!!!!!!!!!!!!!!!!!!!!!!"
     end
   end
 
   def update
     @survey = Survey.find(params[:id])
-    respond_to do |format|
-      if @survey.update(survey_params)
-        format.html { redirect_to @survey, notice: 'Survey was successfully updated.' }
-        format.json { render :show, status: :ok, location: @survey }
-      else
-        format.html { render :edit }
-        format.json { render json: @survey.errors, status: :unprocessable_entity }
-      end
+
+    if @survey.update(survey_params)
+
+    else
+
     end
   end
 
   def destroy
     @survey = Survey.find(params[:id])
     @survey.destroy
-    respond_to do |format|
-      format.html { redirect_to user_surveys_path(@survey.user_id), notice: 'Survey was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    #do something?
   end
 
   private
