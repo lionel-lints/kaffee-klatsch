8cc776cdf40ff04ddfbb594f1c0045b82f808c57
LionelLints <lionel.lints@gmail.com>
2016-01-05 14:24:16 -0800

    update precompile for heroku production with icomoon

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
 Rails.application.config.assets.precompile = %w(homes.js roots.js application.css)
 Rails.application.config.assets.precompile += %w( angulars/home/index.html angulars/home/about.html angulars/root/index.html)
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
